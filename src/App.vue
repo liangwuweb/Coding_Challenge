6af0e34d7fa341574cd69f14d9795fbeeb09cd14
Liang Wu <liangwuweb@gmail.com>
2023-02-13 23:04:42 -0600

    frontend css works

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,60 @@
 <template>
   <div>
-    <div class="container">
-      <form id="main-form" @submit.prevent="handleSubmit">
-        <div class="input-group mb-3">
-          <label class="input-group-text" for="city">City</label>
-          <select class="form-select inline-block" aria-label="Default select example" name="city" id="city" v-model="selectedCity">
-            <option value="milwaukee">Milwaukee</option>
-          </select>
+    <div class="container border-bottom pb-5">
+      <div class="row">
+        <div class="col-md-3"></div>
+
+        <div class="col-md-6">
+          <form id="main-form" @submit.prevent="handleSubmit">
+
+            <div class="input-group mb-3">
+              <label class="input-group-text" for="city">City</label>
+              <select class="form-select inline-block" aria-label="Default select example" name="city" id="city"
+                v-model="selectedCity">
+                <option value="milwaukee">Milwaukee</option>
+              </select>
+            </div>
+
+            <div class="input-group mb-3">
+              <label class="input-group-text" for="type">Type</label>
+              <select class="form-select" aria-label="Default select example" name="type" id="type"
+                v-model="selectedType">
+                <option value="all">All</option>
+                <option v-for="item, index in breweryType" :value="item" :key="index">
+                  {{ item }}
+                </option>
+              </select>
+            </div>
+
+            <button class="btn btn-success" type=submit>Submit</button>
+
+          </form>
         </div>
 
+        <div class="col-md-3"></div>
+      </div>
 
-        <div class="input-group mb-3">
-          <label class="input-group-text" for="type">Type</label>
-          <select class="form-select" aria-label="Default select example" name="type" id="type" v-model="selectedType">
-            <option value="all">All</option>
-            <option v-for="item, index in breweryType" :value="item" :key="index">
-              {{ item }}
-            </option>
-
-            
-          </select>
-        </div>
-        <button class="btn btn-success" type=submit>Submit</button>
-      </form>
     </div>
 
-    
-    <div class="wrapper">
-      <p v-for="item in data" :key="item.id">{{ item.name }}</p>
+    <div class="container mt-5 d-flex flex-wrap">
+      <div v-for="item in data" :key="item.id" class="card m-2" style="width: 18rem;">
+        <div class="card-body">
+          <h5>{{ item.name }}</h5>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">{{ item.brewery_type }}</li>
+            <li class="list-group-item">street: {{ item.street }}</li>
+            <li class="list-group-item">phone: {{ item.phone }}</li>
+          </ul>
+          
+          <a target="_blank" :href="item.website_url != null ?  item.website_url : '#'" :class="item.website_url != null ?  'btn btn-primary' : 'd-none'">website</a> 
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import {ref, onMounted} from 'vue';
+import { ref, onMounted } from 'vue';
 
 
 export default {
@@ -43,27 +64,27 @@ export default {
     let results = ref(null);
     let data = ref(null);
     let selectedCity = ref('milwaukee'),
-        selectedType = ref('all');
+      selectedType = ref('all');
 
-    const breweryType = ['micro', 'nano', 'regional', 'brewpub', 'large', 'planning','bar', 'contract', 'proprietor', 'closed'];
+    const breweryType = ['micro', 'nano', 'regional', 'brewpub', 'large', 'planning', 'bar', 'contract', 'proprietor', 'closed'];
 
     const handleSubmit = () => {
       const type = selectedType.value;
 
       // filter data based on type
-      if (type === 'all') 
+      if (type === 'all')
         fetchData(`https://api.openbrewerydb.org/breweries?by_city=milwaukee`);
-       else 
+      else
         fetchData(`https://api.openbrewerydb.org/breweries?by_city=milwaukee&by_type=${type}`);
-      
+
     }
 
-    const fetchData = async(url) => {
+    const fetchData = async (url) => {
       const response = await fetch(url);
       data.value = await response.json();
     }
 
-    onMounted(async() => {
+    onMounted(async () => {
       const response = await fetch("https://api.openbrewerydb.org/breweries?by_city=milwaukee");
       results.value = await response.json();
     })
@@ -92,8 +113,7 @@ export default {
 }
 
 
-  .form-select {
-    width: 200px !important;
-  }
-
+.form-select {
+  width: 200px !important;
+}
 </style>
