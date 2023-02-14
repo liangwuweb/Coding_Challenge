21f1c8e40795d1e61a17d79674d4415e316d78a0
Liang Wu <liangwuweb@gmail.com>
2023-02-14 09:51:31 -0600

    finish front end and enhance UX

--- src/App.vue
+++ src/App.vue
@@ -36,8 +36,10 @@
 
     </div>
 
-    <div class="container mt-5 d-flex flex-wrap">
-      <div v-for="item in data" :key="item.id" class="card m-2" style="width: 18rem;">
+    <h3 v-if="error" class="alert alert-danger">{{ error }}</h3>
+
+    <div v-if="data != null" class="container mt-5 d-flex flex-wrap">
+      <div  v-for="item in data" :key="item.id" class="card m-2" style="width: 18rem;">
         <div class="card-body">
           <h5>{{ item.name }}</h5>
           <ul class="list-group list-group-flush">
@@ -49,52 +51,56 @@
           <a target="_blank" :href="item.website_url != null ?  item.website_url : '#'" :class="item.website_url != null ?  'btn btn-primary' : 'd-none'">website</a> 
         </div>
       </div>
+      
     </div>
+    <div class="m-3" v-if="data === null">No data yet...</div>
+    <div class="m-3" v-if="data != null && data.length === 0">No data found...</div>
   </div>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
+import { ref } from 'vue';
 
 
 export default {
 
   setup() {
-
-    let results = ref(null);
     let data = ref(null);
     let selectedCity = ref('milwaukee'),
       selectedType = ref('all');
+    let error = ref('');
 
-    const breweryType = ['micro', 'nano', 'regional', 'brewpub', 'large', 'planning', 'bar', 'contract', 'proprietor', 'closed'];
+    const breweryType = ['micro', 'nano', 'regional', 'brewpub', 'large', 'planning', 'bar', 'contract', 'proprieter', 'closed'];
 
+    // event handler for sumbit
     const handleSubmit = () => {
       const type = selectedType.value;
 
       // filter data based on type
       if (type === 'all')
         fetchData(`https://api.openbrewerydb.org/breweries?by_city=milwaukee`);
-      else
+      else {
         fetchData(`https://api.openbrewerydb.org/breweries?by_city=milwaukee&by_type=${type}`);
-
+      }
     }
 
+    // Fetch data from api
     const fetchData = async (url) => {
-      const response = await fetch(url);
-      data.value = await response.json();
+      try {
+        const response = await fetch(url);
+        data.value = await response.json();
+      } catch (err) {
+            console.log(err);
+            error.value = 'invalid type';
+      }
     }
-
-    onMounted(async () => {
-      const response = await fetch("https://api.openbrewerydb.org/breweries?by_city=milwaukee");
-      results.value = await response.json();
-    })
 
     return {
       selectedCity,
-      results,
       selectedType,
       data,
       breweryType,
+      error,
       handleSubmit
     }
   }
@@ -110,10 +116,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-
-.form-select {
-  width: 200px !important;
 }
 </style>
